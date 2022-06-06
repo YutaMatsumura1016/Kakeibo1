87486a701a0d6669ec55ffc98ec3b30530b9bbee
YutaMatsumura1016 <yuta.train1016@gmail.com>
2022-06-06 21:01:40 +0900

    入力周り書き始めた

--- app/src/main/java/com/example/kakeibo1/MainActivity.java
+++ app/src/main/java/com/example/kakeibo1/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.kakeibo1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -18,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList dayArray;
     public ArrayList contentArray;
     public ArrayList paymentArray;
+    private InputMethodManager inputMethodManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,24 +44,48 @@ public class MainActivity extends AppCompatActivity {
         ListView dayList = (ListView) findViewById(R.id.dayList);
         ListView contentList = (ListView) findViewById(R.id.contentList);
         ListView paymentList = (ListView) findViewById(R.id.paymentList);
+        EditText monthIn = (EditText) findViewById(R.id.monthIn);
+        EditText dayIn = (EditText) findViewById(R.id.dayIn);
+        EditText contentIn = (EditText) findViewById(R.id.contentIn);
+        EditText paymentIn = (EditText) findViewById(R.id.paymentIn);
 
-        ArrayAdapter monthArrayAdapter =
-                new ArrayAdapter(
-                        this, R.layout.list, monthArray);
-        ArrayAdapter dayArrayAdapter =
-                new ArrayAdapter(
-                        this, R.layout.list, dayArray);
-        ArrayAdapter contentArrayAdapter =
-                new ArrayAdapter(
-                        this, R.layout.list, contentArray);
-        ArrayAdapter paymentArrayAdapter =
-                new ArrayAdapter(
-                        this, R.layout.list, paymentArray);
+        ArrayAdapter monthArrayAdapter = new ArrayAdapter(this, R.layout.list, monthArray);
+        ArrayAdapter dayArrayAdapter = new ArrayAdapter(this, R.layout.list, dayArray);
+        ArrayAdapter contentArrayAdapter = new ArrayAdapter(this, R.layout.list, contentArray);
+        ArrayAdapter paymentArrayAdapter = new ArrayAdapter(this, R.layout.list, paymentArray);
 
         monthList.setAdapter(monthArrayAdapter);
         dayList.setAdapter(dayArrayAdapter);
         contentList.setAdapter(contentArrayAdapter);
         paymentList.setAdapter(paymentArrayAdapter);
+
+        inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+
+        //入力
+        monthIn.setOnKeyListener(new View.OnKeyListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    String monthText = monthIn.getText().toString();
+                    String dayText = dayIn.getText().toString();
+                    String contentText = contentIn.getText().toString();
+                    String paymentText = paymentIn.getText().toString();
+
+                    if (monthText.length() != 0 && dayText.length() != 0 && contentText.length() != 0 && paymentText.length() != 0) {
+                        try {
+                            InputStream inputStream = getResources().getAssets().open("test1.csv");
+                            convertToCSV(monthText);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });//入力1終わり
     }
 
 
@@ -84,4 +117,27 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    //csvに書き込み
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static void convertToCSV(String monthText) {
+
+        try {
+            //FileWriterインスタンスの生成
+            FileWriter csvWriter = new FileWriter("test1.csv");
+
+            //バッファに追加（行の各項目をカンマ区切りで連結）
+            csvWriter.append(String.join(monthText));
+            //バッファに追加（改行）
+            csvWriter.append("\n");
+
+            //ファイルへの書き込み
+            csvWriter.flush();
+            //ストリームを閉じる
+            csvWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
